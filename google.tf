766175e4bec422efb0822ebb3e8666a47374c13b
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-04-18 17:28:15 -0700

    Add protonmail MX for firezone.dev

--- google.tf
+++ google.tf
@@ -2,7 +2,7 @@ resource "vultr_dns_record" "google-ext-verification" {
   domain = vultr_dns_domain.firezone-dev.id
   name = "ext"
   type = "TXT"
-  data = "google-site-verification=2gUngxQBAdrDvvgXNtomD_eCRmwNMT5t6pEjR1cEhO8"
+  data = "\"google-site-verification=2gUngxQBAdrDvvgXNtomD_eCRmwNMT5t6pEjR1cEhO8\""
 }
 
 resource "vultr_dns_record" "google-ext-mx-1" {
@@ -45,18 +45,18 @@ resource "vultr_dns_record" "google-ext-mx-5" {
   priority = 10
 }
 
-resource "vultr_dns_record" "google-ext-txt-1" {
+resource "vultr_dns_record" "google-ext-dkim" {
   domain = vultr_dns_domain.firezone-dev.id
   name = "google._domainkey.ext"
   type = "TXT"
-  data = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAubhkd+M9O2fILLpfRzCN5vhd81uSfaCbfeQ5Uf/BsBnuJ8AYOsyWbzy3UYU1y2JnJi1D8U+o1idcTPC1wB1okBHUnohI1O9hRDHb5NzV4NTxK0D36ESbgGzv94xu1n1GfxoO/wWga69eu/unz79/SRdVEida09bF0eXg9q5dtyIPI9NvYGtKAvLIABYHkutlUA2dNggraVTXldTlccMWmtd9uzemBg0bpN6zxygSLM9PSsEf0WEJJYvUXrEIQI4o9Ujh1/PqIgRpdqRAbmyhO3BobGNm5qmn3i1ZxWF0LT8zC3QShMPO+BagJlDav1ZNxBtih+vqqeyJvm8gwPXHiQIDAQAB"
+  data = "\"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAubhkd+M9O2fILLpfRzCN5vhd81uSfaCbfeQ5Uf/BsBnuJ8AYOsyWbzy3UYU1y2JnJi1D8U+o1idcTPC1wB1okBHUnohI1O9hRDHb5NzV4NTxK0D36ESbgGzv94xu1n1GfxoO/wWga69eu/unz79/SRdVEida09bF0eXg9q5dtyIPI9NvYGtKAvLIABYHkutlUA2dNggraVTXldTlccMWmtd9uzemBg0bpN6zxygSLM9PSsEf0WEJJYvUXrEIQI4o9Ujh1/PqIgRpdqRAbmyhO3BobGNm5qmn3i1ZxWF0LT8zC3QShMPO+BagJlDav1ZNxBtih+vqqeyJvm8gwPXHiQIDAQAB\""
 }
 
-resource "vultr_dns_record" "google-ext-txt-2" {
+resource "vultr_dns_record" "google-ext-spf" {
   domain = vultr_dns_domain.firezone-dev.id
   name = "ext"
   type = "TXT"
-  data = "v=spf1 include:_spf.google.com ~all"
+  data = "\"v=spf1 include:_spf.google.com ~all\""
 }
 
 resource "vultr_dns_record" "google-external-mx-1" {
@@ -99,16 +99,16 @@ resource "vultr_dns_record" "google-external-mx-5" {
   priority = 10
 }
 
-resource "vultr_dns_record" "google-external-txt-1" {
+resource "vultr_dns_record" "google-external-dkim" {
   domain = vultr_dns_domain.firezone-dev.id
   name = "google._domainkey.external"
   type = "TXT"
-  data = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzN8kOwCTLwyzUnpJ4lhppQ4VwfSbxLlOEIkAVj11OvtQJzJLl11XP7Eql4t3qFo5JVh4pv8Z8K2YyEvlK+dbWp6C1RBLGQtc8v0hJzoot+8SVolJzK0CxBnErTc8etCYkNmJAoiu0NMUacEC7ihYh58ILK8WNgOJCSch4G529Rc1wTHV2fD7MVVsNIdU2Ty++eALgq1pyEIaW4j4yahdIOxgvivPhLpOnEBzB9jSMb8oyAyaiUUXSzGA0ELa6sOHGtCNwtWkcSawD4dCPqj1l4J+9/MhgsDNMyoNf6QUTXWKrjxxggF98lHWWfx9zI7x3gzYJ4eK/e3kdgCyANpA9wIDAQAB"
+  data = "\"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzN8kOwCTLwyzUnpJ4lhppQ4VwfSbxLlOEIkAVj11OvtQJzJLl11XP7Eql4t3qFo5JVh4pv8Z8K2YyEvlK+dbWp6C1RBLGQtc8v0hJzoot+8SVolJzK0CxBnErTc8etCYkNmJAoiu0NMUacEC7ihYh58ILK8WNgOJCSch4G529Rc1wTHV2fD7MVVsNIdU2Ty++eALgq1pyEIaW4j4yahdIOxgvivPhLpOnEBzB9jSMb8oyAyaiUUXSzGA0ELa6sOHGtCNwtWkcSawD4dCPqj1l4J+9/MhgsDNMyoNf6QUTXWKrjxxggF98lHWWfx9zI7x3gzYJ4eK/e3kdgCyANpA9wIDAQAB\""
 }
 
-resource "vultr_dns_record" "google-external-txt-2" {
+resource "vultr_dns_record" "google-external-spf" {
   domain = vultr_dns_domain.firezone-dev.id
   name = "external"
   type = "TXT"
-  data = "v=spf1 include:_spf.google.com ~all"
+  data = "\"v=spf1 include:_spf.google.com ~all\""
 }
