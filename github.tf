bab41f44148798e4176d1b454cab33decc0ab2c2
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-05-13 08:12:31 -0700

    Fix DMARC records for root, ext, and external

--- github.tf
+++ github.tf
@@ -2,5 +2,5 @@ resource "vultr_dns_record" "github-verification" {
   domain = vultr_dns_domain.firezone-dev.id
   name = "_github-challenge-firezone-organization"
   type = "TXT"
-  data = "ca4903847a"
+  data = "\"ca4903847a\""
 }
