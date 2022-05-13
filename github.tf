83006c706974b26a54bf7c0522828d6a7dcdb3ce
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-05-12 18:26:39 -0700

    Add github verification

---
+++ github.tf
@@ -0,0 +1,6 @@
+resource "vultr_dns_record" "github-verification" {
+  domain = vultr_dns_domain.firezone-dev.id
+  name = "_github-challenge-firezone-organization"
+  type = "TXT"
+  data = "ca4903847a"
+}
