dfd3f640dbac7ed2bbb32004e65c66bc2ad2d144
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-06-03 21:01:36 -0600

    Fix outbound emailS

--- sendgrid.tf
+++ sendgrid.tf
@@ -1,20 +1,20 @@
 resource "vultr_dns_record" "sendgrid-dkim-1" {
   domain = vultr_dns_domain.firezone-dev.id
-  name = "em8227.firezone.dev"
+  name = "em8227"
   data = "u23539796.wl047.sendgrid.net"
   type = "CNAME"
 }
 
 resource "vultr_dns_record" "sendmail-dkim-2" {
   domain = vultr_dns_domain.firezone-dev.id
-  name = "s1._domainkey.firezone.dev"
+  name = "s1._domainkey"
   data = "s1.domainkey.u23539796.wl047.sendgrid.net"
   type = "CNAME"
 }
 
 resource "vultr_dns_record" "sendmail-dkim-3" {
   domain = vultr_dns_domain.firezone-dev.id
-  name = "s2._domainkey.firezone.dev"
+  name = "s2._domainkey"
   data = "s2.domainkey.u23539796.wl047.sendgrid.net"
   type = "CNAME"
 }
