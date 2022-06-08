7731db56413daca2f454ffe6ef26449e0b605e30
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-06-08 08:17:32 -0700

    Fix typo

--- ping-dev.tf
+++ ping-dev.tf
@@ -1,6 +1,6 @@
 resource "vultr_dns_record" "ping-dev_acme" {
   domain = vultr_dns_domain.firezone-dev.id
-  name = "_acme-challenge.ping-dev-firez.one"
+  name = "_acme-challenge.ping-dev.firez.one"
   data = "WgL_bm02A_qec7eoYcp249-q9WLk0I-1F_J7tbV7GKA"
   type = "TXT"
 }
